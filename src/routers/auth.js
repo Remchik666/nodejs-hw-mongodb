b3a6dccd6ab755f831e6ee74d9f53465e3990673
Remchik <art6000.7m@gmail.com>
2025-10-25 12:41:31 +0300

    final commit

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,16 +1,44 @@
 import express from 'express';
-import { registerUserController, loginUserController, logoutUserController, refreshSessionController, requestResetEmailController, resetPasswordController } from '../controllers/auth.js';
+import {
+  registerUserController,
+  loginUserController,
+  logoutUserController,
+  refreshSessionController,
+  requestResetEmailController,
+  resetPasswordController,
+} from '../controllers/auth.js';
 import { validateBody } from '../middlewares/validateBody.js';
-import { registerSchema, loginSchema, requestResetEmailSchema, resetPasswordSchema } from '../validation/auth.js';
-import { ctrlWrapper } from "../utils/ctrlWrapper.js";
+import {
+  registerSchema,
+  loginSchema,
+  requestResetEmailSchema,
+  resetPasswordSchema,
+} from '../validation/auth.js';
+import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 
 const router = express.Router();
 
-router.post("/register", validateBody(registerSchema), ctrlWrapper(registerUserController));
-router.post("/login", validateBody(loginSchema), ctrlWrapper(loginUserController));
-router.post("/logout", ctrlWrapper(logoutUserController));
-router.post("/refresh", ctrlWrapper(refreshSessionController));
-router.post("/send-reset-email", validateBody(requestResetEmailSchema), ctrlWrapper(requestResetEmailController));
-router.post("/reset-password", validateBody(resetPasswordSchema), ctrlWrapper(resetPasswordController));
+router.post(
+  '/register',
+  validateBody(registerSchema),
+  ctrlWrapper(registerUserController),
+);
+router.post(
+  '/login',
+  validateBody(loginSchema),
+  ctrlWrapper(loginUserController),
+);
+router.post('/logout', ctrlWrapper(logoutUserController));
+router.post('/refresh', ctrlWrapper(refreshSessionController));
+router.post(
+  '/send-reset-email',
+  validateBody(requestResetEmailSchema),
+  ctrlWrapper(requestResetEmailController),
+);
+router.post(
+  '/reset-pwd',
+  validateBody(resetPasswordSchema),
+  ctrlWrapper(resetPasswordController),
+);
 
 export default router;
--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,9 +1,18 @@
 import express from 'express';
-import { getContactsController, getContactByIdController, createContactController, deleteContactController, updateContactController } from '../controllers/contacts.js';
+import {
+  getContactsController,
+  getContactByIdController,
+  createContactController,
+  deleteContactController,
+  updateContactController,
+} from '../controllers/contacts.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import { isValidId } from '../middlewares/isValidId.js';
 import { validateBody } from '../middlewares/validateBody.js';
-import { createContactSchema, updateContactSchema } from '../validation/contact.js';
+import {
+  createContactSchema,
+  updateContactSchema,
+} from '../validation/contact.js';
 import { upload } from '../middlewares/upload.js';
 
 const router = express.Router();
@@ -12,12 +21,21 @@ router.get('/', ctrlWrapper(getContactsController));
 
 router.get('/:id', isValidId, ctrlWrapper(getContactByIdController));
 
-router.post("/", upload.single("photo"), validateBody(createContactSchema), ctrlWrapper(createContactController));
+router.post(
+  '/',
+  upload.single('photo'),
+  validateBody(createContactSchema),
+  ctrlWrapper(createContactController),
+);
 
-router.delete("/:id", isValidId, ctrlWrapper(deleteContactController));
+router.delete('/:id', isValidId, ctrlWrapper(deleteContactController));
 
-router.patch("/:id", upload.single("photo"), validateBody(updateContactSchema), isValidId, ctrlWrapper(updateContactController));
-
-
+router.patch(
+  '/:id',
+  upload.single('photo'),
+  validateBody(updateContactSchema),
+  isValidId,
+  ctrlWrapper(updateContactController),
+);
 
 export default router;
